724a26d1b408672faa2b9a6002da2590bf99fc29
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-11 16:57:04 +0330

    change style [CardBlog]

--- src/modules/home/cardBlog/index.jsx
+++ src/modules/home/cardBlog/index.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const CardBlog = ({ item }) => {
   return (
-    <Link className="w-full h-full" href={`blog/${item?.id}`}>
+    <Link className="w-full max-w-[720px] h-full" href={`blog/${item?.id}`}>
       <div
         key={item.id}
         className="p-4 w-full border-b border-tc_gray cursor-pointer flex flex-col gap-2"
       >
-        <div className="w-full h-full p-2 border-2 rounded-lg">
-          {/* <img src={item.img} className="w-full" /> */}
+        <div className="w-full h-full p-2 border-2 rounded-xl">
+          <img src={item.img} className="w-full rounded-lg" />
         </div>
         <div className="flex justify-between">
           <p>{item.title}</p>
