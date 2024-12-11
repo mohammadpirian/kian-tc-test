41e4926bca0c2cdd88f183beadb3bec5df211f05
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-11 16:22:05 +0330

    create blog page

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,19 @@
+// "use client";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  return (
+    <div className="w-full h-full py-12 ">
+      <div className="w-full h-full flex flex-col items-center gap-4">
+        <div className="w-[720px] h-[480px] border-2 p-2 rounded-xl">
+          <img src="/images/1.jpg" className="w-full h-full rounded-lg" />
+        </div>
+        <p className="text-lg font-bold">{id}</p>
+      </div>
+    </div>
+  );
+}
