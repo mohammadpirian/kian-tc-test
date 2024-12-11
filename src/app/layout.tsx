fd52a5ead5a8974da765b7cd07a44522ff2e5553
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-11 16:21:22 +0330

    add div for children [RootLayout]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <div className="w-full min-h-screen pt-52">{children}</div>
       </body>
     </html>
   );
