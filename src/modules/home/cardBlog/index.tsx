b6c1f773e3ec644d5612eacabad961581c7ab847
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-14 17:39:26 +0330

    change file

--- src/modules/home/cardBlog/index.tsx
+++ src/modules/home/cardBlog/index.tsx
@@ -9,7 +9,7 @@ const CardBlog = ({ item }) => {
         className="p-4 w-full border-b border-tc_gray cursor-pointer flex flex-col gap-2"
       >
         <div className="w-full h-full p-2 border-2 rounded-xl">
-          <img src={item.img} className="w-full rounded-lg" />
+          {/* <img src={item.img} className="w-full rounded-lg" /> */}
         </div>
         <div className="flex justify-between">
           <p>{item.title}</p>
