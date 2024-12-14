a3e017c3ba11d785f5a9fb1e4d2595c3ebbc51db
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-14 17:40:06 +0330

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
-import Header from "@/modules/layout/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Kian TC",
@@ -25,12 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-y-hidden`}
-      >
-        <Header />
-        <div className="w-full h-full pt-52">{children}</div>
-      </body>
+      <body className={` antialiased overflow-y-hidden`}>{children}</body>
     </html>
   );
 }
