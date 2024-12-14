2f26357bea81243e2bf8a640697a11a37aae557a
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-14 17:42:42 +0330

    create BlogModal

---
+++ src/app/blog/@modal/(..)blog/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Modal from "@/components/Modal";
+import Image from "next/image";
+
+export default function BlogModal() {
+  console.log("BlogModal");
+  // console.log("id", id);
+  console.log(
+    "image",
+    "https://www.kiantc.com/images/content/Group-620913378.webp"
+  );
+
+  return (
+    <>
+      <Image
+        alt=""
+        src={"https://www.kiantc.com/images/content/Group-620913378.webp"}
+        height={600}
+        width={600}
+        className="col-span-1 aspect-square w-full object-cover"
+      />
+
+      <div className=" bg-white p-2 px-4">
+        {/* <h3 className="font-serif text-xl font-medium">{id}</h3> */}
+        <p className="text-sm text-gray-500">Taken by Mohammad</p>
+      </div>
+    </>
+  );
+}
