51b66467a7a306ecd5171a23054f62772136ef3e
mohammad piriyan <mohammadpiriyan@gmail.com>
2024-12-14 17:46:30 +0330

    create PhotoCard

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,4 @@
-// "use client";
-
-export default async function Page({
+export default async function BlogPage({
   params,
 }: {
   params: Promise<{ id: string }>;
@@ -10,7 +8,7 @@ export default async function Page({
     <div className="w-full h-full py-12 ">
       <div className="w-full h-full flex flex-col items-center gap-4">
         <div className="w-[720px] h-[480px] border-2 p-2 rounded-xl">
-          <img src="/images/1.jpg" className="w-full h-full rounded-lg" />
+          {/* <img src="/images/1.jpg" className="w-full h-full rounded-lg" /> */}
         </div>
         <p className="text-lg font-bold">{id}</p>
       </div>
